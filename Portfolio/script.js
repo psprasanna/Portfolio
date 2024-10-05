730d24a7c4bf02b378a9eba8d8311aa1b2973ff6
psprasanna <psprasanna3011@gmail.com>
2024-10-06 00:53:57 +0530

    animations added

---
+++ Portfolio/script.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".multiple-text",{
+  strings: ["Software Developer","Frontend Developer","Backend Developer"],
+  typeSpeed:100,
+  backSpeed:100,
+  backDelay:1000,
+  loop:true
+});
